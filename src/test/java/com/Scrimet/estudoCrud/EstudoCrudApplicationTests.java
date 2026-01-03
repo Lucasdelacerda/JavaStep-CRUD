249f4d2235d99b919c8c8867ca5df6d8e94394c3
Lucas de Lacerda <106289445+Lucasdelacerda@users.noreply.github.com>
2026-01-03 11:46:49 -0300

    Initial commit

---
+++ src/test/java/com/Scrimet/estudoCrud/EstudoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Scrimet.estudoCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
